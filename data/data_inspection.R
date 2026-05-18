f6f6d158032a49e46d5539a73ab406652bd240d8
rithrithh <mdha0019@student.monash.edu>
2026-05-19 02:02:45 +0800

    Prodcer Price Index over time

--- data/data_inspection.R
+++ data/data_inspection.R
@@ -5,3 +5,13 @@ df <- read.csv("GoogleReview_data_cleaned.csv")
 unique_locations <- unique(df$Location)
 
 print(unique_locations)
+
+df_Penang <- df %>% filter(Location == "Penang")
+
+df_miri <- df %>% filter(Location == "Miri")
+
+df <- read.csv("producer-prices_mys.csv")
+
+unique_foods <- unique(df$Item)
+
+print(unique_foods)
